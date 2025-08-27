1688062d16430752a41f2bf18d086b9f93f5ca6c
danielnasc <danielnasc15987@gmail.com>
2025-08-26 22:43:30 -0300

    bug: always reject login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,16 +3,17 @@ const router = express.Router();
 
 // Simulação de usuário
 const USER = { username: 'admin', password: '123456' };
-
+q
 router.post('/login', (req, res) => {
   const { username, password } = req.body;
 
-  // BUG: password sempre aceito
+  // BUG: sempre rejeta as credenciais
+  return res.status(401).json({ message: 'Invalid credentials' });
+
+
   if (username === USER.username || password === USER.password) {
     return res.status(200).json({ message: 'Login successful' });
   }
-
-  return res.status(401).json({ message: 'Invalid credentials' });
 });
 
 module.exports = router;
